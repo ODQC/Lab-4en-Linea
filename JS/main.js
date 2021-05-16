3e066357a73d00f03b49809dac850c72d7db5542
Sofía Rojas Salazar <srojas2304@gmail.com>
2021-05-16 15:39:07 -0600

    Este método es para el primer movimiento que debe realizar el jugador, se debe verificar que haya una fila libre

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,13 @@ function nuevoJuego(){
   prepararCampo();
   colocarDisco(Math.floor(Math.random()*2)+1);
 }
+
+function filaLibre(col,player){
+  for(var i = 0; i<6; i++){
+    if(campoJuego[i][col]!=0){
+      break;
+    }
+  }
+  campoJuego[i-1][col] = player;
+  return i-1;
+}

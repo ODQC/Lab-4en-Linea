5a93ae791905447953e00bf2df2e4272d56151da
Sofía Rojas Salazar <srojas2304@gmail.com>
2021-05-16 11:23:40 -0600

    Crea la nueva matriz donde se iniciará la partida

--- JS/main.js
+++ JS/main.js
@@ -11,3 +11,13 @@ function nuevoJuego(){
   prepararCampo();
   colocarDisco(Math.floor(Math.random()*2)+1);
 }
+function prepararCampo(){
+  campoJuego = new Array();
+
+  for(var i=0; i<6; i++){
+    campoJuego[i] = new Array();
+    for(var j=0; j<7; j++){
+      campoJuego[i].push(0);
+    }
+  }
+}
